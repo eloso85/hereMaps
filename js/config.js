3cdd1d19385cc6139661841357707f04cc8674b9
Alejandro <eloso85@gmail.com>
2021-04-01 23:23:12 -0500

    added berlin

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
+import hereCredentials from '../hereConfig.js'
+
 const center = { 
    lat: 52.5159, 
    lng: 13.3777, 
    text: 'Berlin, Germany'
 }
 
-const hereCredentials = {
-   id: 'YOUR-HERE-ID',
-   code: 'YOUR-HERE-CODE',
-   apikey: 'YOUR-HERE-APIKEY'
-}
+
+
+//console.log(hereCredentials)
+
 
 export { center, hereCredentials };
